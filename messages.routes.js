428dc46df9bcae44cea8a8a3ee1ff6ad05894b84
jpena092 <jpena092@jaguar.tamu.edu>
2026-04-20 12:39:39 -0500

    messages.routes.js

---
+++ messages.routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const Message = require('../models/Message');
+const auth = require('../middleware/auth');
+
+router.post('/send', auth, async (req, res) => {
+  const { receiverId, encryptedContent } = req.body;
+
+  const message = new Message({
+    senderId: req.user.id,
+    receiverId,
+    encryptedContent
+  });
+
+  await message.save();
+  res.json(message);
+});
+
+router.get('/:userId', auth, async (req, res) => {
+  const messages = await Message.find({
+    $or: [
+      { senderId: req.user.id, receiverId: req.params.userId },
+      { senderId: req.params.userId, receiverId: req.user.id }
+    ]
+  });
+
+  res.json(messages);
+});
+
+module.exports = router;
