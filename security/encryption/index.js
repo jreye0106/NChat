a44b0e66449c1caece9d66c94482a2a84a2b9b71
MikalCelis <147455350+MikalCelis@users.noreply.github.com>
2026-04-29 12:54:48 -0500

    Add encryption module exports

---
+++ security/encryption/index.js
@@ -0,0 +1,9 @@
+const generateKeyPair = require("./generateKeyPair");
+const encryptMessage = require("./encryptMessage");
+const decryptMessage = require("./decryptMessage");
+
+module.exports = {
+  generateKeyPair,
+  encryptMessage,
+  decryptMessage,
+};
